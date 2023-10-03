04e1ae2e741e3d219ae51d49d939313a0e1e4ddd
Mehul Motani <48776133+mehulmotani@users.noreply.github.com>
2023-10-04 00:14:12 +0800

    update

--- topbar.js
+++ topbar.js
@@ -1,4 +1,4 @@
-document.write('<br> Type|<a href="pubs-JourSubPub.html">Journal</a>| |<a href="pubs-ConfSel.html">Selected Conference</a>| |<a href="pubs-ConfOther.html">Other Conferences</a>| |<a href="pubs-BookChap.html">Book Chapters</a>| |<a href="pubs-TechRep.html">Technical Reports</a>| <br> Topic |<a href="pubs-ML.html">AI & ML</a>| |<a href="pubs-Bio.html">Digital Health & AI In Healthcare</a>| |<a href="pubs-Comms.html">Communication & Information Theory</a>| <br>')
+document.write('<br> Type|<a href="pubs-JourSubPub.html">Journal</a>| |<a href="pubs-ConfSel.html">Selected Conference</a>| |<a href="pubs-ConfOther.html">Other Conferences</a>| |<a href="pubs-BookChap.html">Book Chapters</a>| |<a href="pubs-TechRep.html">Technical Reports</a>| <br> Topic |<a href="pubs-ML.html">AI & Mchine Learning</a>| |<a href="pubs-Bio.html">Digital Health & AI In Healthcare</a>| |<a href="pubs-Comms.html">Communication & Information Theory</a>| <br>')
 
 
 
